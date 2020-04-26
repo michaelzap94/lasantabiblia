abf57eea791d77c033a4a1944c7776db9bb30a86
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-26 01:39:37 +0100

    added create fragment

--- app/src/main/java/com/michaelzap94/santabiblia/DatabaseHelper/ContentDBHelper.java
+++ app/src/main/java/com/michaelzap94/santabiblia/DatabaseHelper/ContentDBHelper.java
@@ -1,8 +1,18 @@
 package com.michaelzap94.santabiblia.DatabaseHelper;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
+
+import com.michaelzap94.santabiblia.models.Label;
+import com.michaelzap94.santabiblia.models.Verse;
+
+import java.util.ArrayList;
 
 public class ContentDBHelper extends SQLiteOpenHelper {
     private static int DATABASE_VERSION = 1;
@@ -14,14 +24,50 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         this.context = context;
     }
 
+    public long createLabel(String name, String color){
+        ContentValues cv = new ContentValues();
+        cv.put("name", name);
+        cv.put("color", color);
+        return this.getReadableDatabase().insert("labels",null, cv);
+    }
+
+    public ArrayList<Label> getAllLabels(){
+        Cursor innerCursor;
+        int rowCount;
+        int i;
+        ArrayList<Label> list = new ArrayList();
+        try {
+            String query = "SELECT * FROM labels";
+            innerCursor = this.getReadableDatabase().rawQuery(query, null);
+            if (innerCursor.moveToFirst()) {
+                rowCount = innerCursor.getCount();
+                for (i = 0; i < rowCount; i++) {
+                    int nameCol = innerCursor.getColumnIndex("name");
+                    int colorCol = innerCursor.getColumnIndex("color");
+
+                    String name = innerCursor.getString(nameCol);
+                    String color = innerCursor.getString(colorCol);
+
+                    list.add(new Label(name, color));
+                    innerCursor.moveToNext();
+                }
+            }
+            innerCursor.close();
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE label (id INTEGER PRIMARY KEY, name VARCHAR NOT NULL, color VARCHAR NOT NULL)");
-        db.execSQL("CREATE TABLE verses_selected (id INTEGER PRIMARY KEY, label INTEGER NOT NULL, book_number INTEGER NOT NULL, chapter INTEGER NOT NULL, verseFrom INTEGER NOT NULL, verseTo INTEGER NOT NULL, color VARCHAR NOT NULL, text VARCHAR, date_created datetime default current_timestamp, date_updated datetime default current_timestamp, state INTEGER NOT NULL)");
+        db.execSQL("CREATE TABLE labels (_id INTEGER PRIMARY KEY, name VARCHAR NOT NULL, color VARCHAR NOT NULL)");
+        db.execSQL("CREATE TABLE verses_marked (_id INTEGER PRIMARY KEY, label INTEGER NOT NULL, book_number INTEGER NOT NULL, chapter INTEGER NOT NULL, verseFrom INTEGER NOT NULL, verseTo INTEGER NOT NULL, color VARCHAR NOT NULL, text VARCHAR, date_created datetime default current_timestamp, date_updated datetime default current_timestamp, state INTEGER NOT NULL)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS labels");
+        db.execSQL("DROP TABLE IF EXISTS verses_marked");
+        onCreate(db);
     }
 }
--- app/src/main/java/com/michaelzap94/santabiblia/fragments/dashboard/DashboardCreatorFragment.java
+++ app/src/main/java/com/michaelzap94/santabiblia/fragments/dashboard/DashboardCreatorFragment.java
@@ -2,11 +2,15 @@ package com.michaelzap94.santabiblia.fragments.dashboard;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.michaelzap94.santabiblia.R;
 
@@ -15,15 +19,36 @@ import com.michaelzap94.santabiblia.R;
  */
 public class DashboardCreatorFragment extends Fragment {
 
+    private EditText name;
+    private EditText color;
+    private Button createButton;
+
     public DashboardCreatorFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.dashboard_creator_fragment, container, false);
+        View view =  inflater.inflate(R.layout.dashboard_creator_fragment, container, false);
+
+        name = view.findViewById(R.id.dash_creator_fragment_name_edittext);
+        color = view.findViewById(R.id.dash_creator_fragment_color);
+        createButton = view.findViewById(R.id.dash_creator_fragment_button);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (name.getText().toString().trim().equalsIgnoreCase("")) {
+                    name.setError("This field can not be blank");
+                }
+                if (color.getText().toString().trim().equalsIgnoreCase("")) {
+                    color.setError("This field can not be blank");
+                }
+
+            }
+        });
+
+        return view;
     }
+
 }
