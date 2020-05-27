940edf3433b99634ebfceff21c6bd469797af111
Michael Zapata <mikeyfriends@hotmail.com>
2020-05-27 15:30:51 +0100

    added feature to only draw delete icon on swipe

--- app/src/main/java/com/michaelzap94/santabiblia/utilities/SwipeToDelete.java
+++ app/src/main/java/com/michaelzap94/santabiblia/utilities/SwipeToDelete.java
@@ -8,6 +8,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.michaelzap94.santabiblia.R;
 
 public abstract class SwipeToDelete extends ItemTouchHelper.Callback {
+        private static final String TAG = "SwipeToDelete";
         //        private boolean swipeBack = false;
         Context mContext;
         private Paint mClearPaint;
@@ -68,36 +70,33 @@ public abstract class SwipeToDelete extends ItemTouchHelper.Callback {
 
         @Override
         public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
 
-            View itemView = viewHolder.itemView;
-            int itemHeight = itemView.getHeight();
+            if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                View itemView = viewHolder.itemView;
+                int itemHeight = itemView.getHeight();
 
-            boolean isCancelled = dX == 0 && !isCurrentlyActive;
+                boolean isCancelled = (dY > 0) || (dX == 0 && !isCurrentlyActive);
 
-            if (isCancelled) {
-                clearCanvas(c, itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
-                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-                return;
+                if (isCancelled) {
+                    clearCanvas(c, itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                    return;
+                }
+
+                mBackground.setColor(backgroundColor);
+                mBackground.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                mBackground.draw(c);
+
+                int deleteIconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+                int deleteIconMargin = (itemHeight - intrinsicHeight) / 2;
+                int deleteIconLeft = itemView.getRight() - deleteIconMargin - intrinsicWidth;
+                int deleteIconRight = itemView.getRight() - deleteIconMargin;
+                int deleteIconBottom = deleteIconTop + intrinsicHeight;
+
+                deleteDrawable.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom);
+                deleteDrawable.draw(c);
             }
-
-            mBackground.setColor(backgroundColor);
-            mBackground.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
-            mBackground.draw(c);
-
-            int deleteIconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
-            int deleteIconMargin = (itemHeight - intrinsicHeight) / 2;
-            int deleteIconLeft = itemView.getRight() - deleteIconMargin - intrinsicWidth;
-            int deleteIconRight = itemView.getRight() - deleteIconMargin;
-            int deleteIconBottom = deleteIconTop + intrinsicHeight;
-
-
-            deleteDrawable.setBounds(deleteIconLeft, deleteIconTop, deleteIconRight, deleteIconBottom);
-            deleteDrawable.draw(c);
-
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-
-
         }
 
         private void clearCanvas(Canvas c, Float left, Float top, Float right, Float bottom) {
@@ -107,7 +106,7 @@ public abstract class SwipeToDelete extends ItemTouchHelper.Callback {
 
         @Override
         public float getSwipeThreshold(@NonNull RecyclerView.ViewHolder viewHolder) {
-            return 0.7f;//We’ve set the Swipe threshold to 0.7. That means if the row is swiped less than 70%, the onSwipe method won’t be triggered.
+            return 0.6f;//We’ve set the Swipe threshold to 0.6. That means if the row is swiped less than 60%, the onSwipe method won’t be triggered.
         }
 
 //        // SwipeController.java
@@ -119,33 +118,5 @@ public abstract class SwipeToDelete extends ItemTouchHelper.Callback {
 //            }
 //            return super.convertToAbsoluteDirection(flags, layoutDirection);
 //        }
-//
-//        // SwipeController.java
-//        @Override
-//        public void onChildDraw(Canvas c,
-//                                RecyclerView recyclerView,
-//                                RecyclerView.ViewHolder viewHolder,
-//                                float dX, float dY,
-//                                int actionState, boolean isCurrentlyActive) {
-//
-//            if (actionState == ACTION_STATE_SWIPE) {
-//                setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-//            }
-//            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-//        }
-//
-//        private void setTouchListener(Canvas c,
-//                                      RecyclerView recyclerView,
-//                                      RecyclerView.ViewHolder viewHolder,
-//                                      float dX, float dY,
-//                                      int actionState, boolean isCurrentlyActive) {
-//
-//            recyclerView.setOnTouchListener(new View.OnTouchListener() {
-//                @Override
-//                public boolean onTouch(View v, MotionEvent event) {
-//                    swipeBack = event.getAction() == MotionEvent.ACTION_CANCEL || event.getAction() == MotionEvent.ACTION_UP;
-//                    return false;
-//                }
-//            });
-//        }
+
 }
