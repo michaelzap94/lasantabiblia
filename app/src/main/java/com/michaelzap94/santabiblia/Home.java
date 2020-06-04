1a56d611ebd8236ab73c43360c831874cc4083b7
Michael Zapata <mikeyfriends@hotmail.com>
2020-06-04 23:40:56 +0100

    added home activity

---
+++ app/src/main/java/com/michaelzap94/santabiblia/Home.java
@@ -0,0 +1,14 @@
+package com.michaelzap94.santabiblia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
