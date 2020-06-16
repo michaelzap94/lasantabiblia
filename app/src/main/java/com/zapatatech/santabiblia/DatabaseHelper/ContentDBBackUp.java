54826a42723cf63415f21c7d9a074e3b141c41b2
Michael Zapata <mikeyfriends@hotmail.com>
2020-06-16 20:19:42 +0100

    content provider

--- app/src/main/java/com/zapatatech/santabiblia/DatabaseHelper/ContentDBBackUp.java
+++ app/src/main/java/com/zapatatech/santabiblia/DatabaseHelper/ContentDBBackUp.java
@@ -17,45 +17,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-import static com.zapatatech.santabiblia.DatabaseHelper.ContentDBContracts.DB_NAME;
-import static com.zapatatech.santabiblia.DatabaseHelper.ContentDBContracts.DB_VERSION;
-
-public class ContentDBHelper extends SQLiteOpenHelper {
-    private static final String TAG = "ContentDBHelper";
+class ContentDBBackUp extends SQLiteOpenHelper {
+    private static final String TAG = "ContentDBBackUp";
+    private static int DATABASE_VERSION = 1;
+    private static String DB_NAME = "content.db";
     private Context context;
-    private static ContentDBHelper dbHelperSingleton = null;
-    private final SQLiteDatabase db;
+    private static ContentDBBackUp dbHelperSingleton = null;
 
-    /**
-     * We use a Singleton to prevent leaking the SQLiteDatabase or Context.
-     * @return {@link ContentDBHelper}
-     */
-    public static ContentDBHelper getInstance(Context c) {
-        if (dbHelperSingleton == null) {
-            synchronized (ContentDBHelper.class) {
-                if (dbHelperSingleton == null) {
-                    dbHelperSingleton = new ContentDBHelper(c);
-                }
-            }
+    public static synchronized ContentDBBackUp getInstance(Context context) {
+        ContentDBBackUp dbHelperInner;
+        synchronized (ContentDBBackUp.class) {
+            if (dbHelperSingleton == null) {
+                //if not singleton, create one.
+                dbHelperSingleton = new ContentDBBackUp(context);
+            }//else return existing one.
+            dbHelperInner = dbHelperSingleton;
         }
-        return dbHelperSingleton;
+        return dbHelperInner;
     }
-    public ContentDBHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
+    public ContentDBBackUp(Context context) {
+        super(context, DB_NAME, null, DATABASE_VERSION);
         this.context = context;
-        this.db = getWritableDatabase();
     }
     public boolean createLabel(String name, String color){
         ContentValues cv = new ContentValues();
         cv.put("name", name);
         cv.put("color", color);
-        return this.db.insert("labels",null, cv) > 0;
+        return this.getWritableDatabase().insert("labels",null, cv) > 0;
     }
     public boolean editLabel(String name, String color, int id){
         ContentValues cv = new ContentValues();
         cv.put("name", name);
         cv.put("color", color);
-        return this.db.update("labels", cv, "_id="+id, null) > 0;
+        return this.getWritableDatabase().update("labels", cv, "_id="+id, null) > 0;
     }
     public ArrayList<Label> getAllLabels(){
         Cursor innerCursor;
@@ -87,7 +81,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         return list;
     }
     public boolean deleteOneLabel(int id){
-         return this.db.delete("labels", "_id =" + id, null) > 0;
+        return this.getWritableDatabase().delete("labels", "_id =" + id, null) > 0;
     }
     public boolean insertSelectedItemsBulkTransaction(String uuid, Label label, int book_number, int chapter_number, String note, List<Integer> selectedItems) {
         boolean success = true;
@@ -95,7 +89,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         String label_name = label.getName();
         String label_color = label.getColor();
         List<List<Integer>> versesGroups = BookHelper.getVersesSelectedResults(selectedItems);//[[1,2,3],[6,7],[9]]
-        SQLiteDatabase db = this.db;
+        SQLiteDatabase db = this.getWritableDatabase();
         db.beginTransaction();
         String uniqueID = (uuid != null) ? uuid : UUID.randomUUID().toString();
         try{
@@ -172,7 +166,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         cv.put("verseFrom", verseFrom);
         cv.put("verseTo", (verseTo));
         cv.put("note", (!note.equals("") ?  note : "NULL"));
-        return this.db.insert("verses_marked",null, cv) > 0;
+        return this.getWritableDatabase().insert("verses_marked",null, cv) > 0;
     }
     public ArrayList<VersesMarked> getVersesMarked(int label_id, String _uuid, int learned) {
         int labelSpecificRowsCount;
@@ -218,10 +212,10 @@ public class ContentDBHelper extends SQLiteOpenHelper {
                     Label specificLabel = new Label(label_id, label_name, label_color, label_permanent);
                     Book specificBook = BookHelper.getBook(book_number);
                     String innerQuery = "SELECT verse , text FROM verses WHERE " +
-                                                                        "book_number = "+book_number+" AND " +
-                                                                        "chapter = "+chapter+" AND " +
-                                                                        "verse BETWEEN "+verseFrom+" AND "+verseTo+" " +
-                                                                        "ORDER BY book_number, chapter, verse";
+                            "book_number = "+book_number+" AND " +
+                            "chapter = "+chapter+" AND " +
+                            "verse BETWEEN "+verseFrom+" AND "+verseTo+" " +
+                            "ORDER BY book_number, chapter, verse";
 //                    if(verseFrom == verseTo){
 //                        innerQuery = "SELECT verse , text FROM verses WHERE book_number = ? AND chapter = ? ORDER BY book_number, chapter, verse";
 //                    } else {
@@ -251,7 +245,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
                     labelSpecificRows.moveToNext();
                 }
             }
-            } catch (Exception e){
+        } catch (Exception e){
         }
         return list;
     }
@@ -265,7 +259,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
     }
     public boolean deleteVersesMarkedGroup(int label_id, String uuid){
         boolean success = false;
-        SQLiteDatabase db = this.db;
+        SQLiteDatabase db = this.getWritableDatabase();
         int rowsDeletedVersesMarked = db.delete("verses_marked", "label_id = ? AND UUID = ?", new String[]{String.valueOf(label_id), uuid});
         if(rowsDeletedVersesMarked > 0 && label_id == CommonMethods.LABEL_ID_MEMORIZE) {
             success = deleteVersesLearned(db, uuid);
@@ -286,7 +280,8 @@ public class ContentDBHelper extends SQLiteOpenHelper {
     public boolean editVersesLearned(String uuid, int learned){
         ContentValues cv = new ContentValues();
         cv.put("learned", learned);
-        return this.db.update("verses_learned", cv, "UUID=?", new String[]{uuid}) > 0;
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.update("verses_learned", cv, "UUID=?", new String[]{uuid}) > 0;
     }
     public boolean insertVersesLearned(String uuid, int learned){
         boolean success = false;
@@ -294,7 +289,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         cv.put("UUID", uuid);
         cv.put("learned", learned);
         cv.put("label_id", CommonMethods.LABEL_ID_MEMORIZE);
-        SQLiteDatabase db = this.db;
+        SQLiteDatabase db = this.getWritableDatabase();
         int rowsUpdated = db.update("verses_learned", cv, "UUID=?", new String[]{uuid});
         if(rowsUpdated <= 0) {
             success = db.insert("verses_learned",null, cv) > 0;
@@ -304,7 +299,7 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         return success;
     }
     public boolean deleteVersesLearned(SQLiteDatabase _db, String uuid){
-        SQLiteDatabase db = (_db == null) ? this.db : _db;
+        SQLiteDatabase db = (_db == null) ? this.getWritableDatabase() : _db;
         return db.delete("verses_learned", "UUID='" + uuid + "'", null) > 0;
     }
     public ArrayList<VersesMarked> getVersesMarkedLearned(int learned){
@@ -321,6 +316,86 @@ public class ContentDBHelper extends SQLiteOpenHelper {
         return -1;
     }
 
+
+    //SEARCH IN NOTES========================================================================================
+//    public ArrayList<SearchResult> searchInNotes(String input) {
+//        ArrayList<SearchResult> results = new ArrayList<>();
+//        int labelSpecificRowsCount;
+//        try {
+//            String query = "SELECT * FROM verses_marked " +
+//                    " WHERE REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(note,'\u00C1','A'), '\u00C9','E'),'\u00CD','I'),'\u00D3','O'),'\u00DA','U'),'\u00e1','a'), '\u00e9','e'),'\u00ed','i'),'\u00f3','o'),'\u00fa','u'),'.',''),':',''),';',''),'?',''),'\u00bf',''),'\u00a1',''),'!',''),'(',''),')','') " +
+//                    " LIKE '%" + input.trim() + "%' ORDER BY topic, definition";
+//            //String query = "SELECT * FROM dictionary WHERE topic LIKE '%" + input.toUpperCase() + "%' ORDER BY topic, definition COLLATE UNICODE";
+//            Cursor labelSpecificRows = this.getReadableDatabase().rawQuery(query, null);
+//            if (labelSpecificRows.moveToFirst()) {
+//                labelSpecificRowsCount = labelSpecificRows.getCount();
+//                for (int i = 0; i < labelSpecificRowsCount; i++) {
+//                    int _idCol= labelSpecificRows.getColumnIndex("_id");
+//                    int uuidCol = labelSpecificRows.getColumnIndex("UUID");
+//                    int label_nameCol= labelSpecificRows.getColumnIndex("label_name");
+//                    int label_colorCol= labelSpecificRows.getColumnIndex("label_color");
+//                    int label_perCol= labelSpecificRows.getColumnIndex("label_permanent");
+//                    int book_numberCol= labelSpecificRows.getColumnIndex("book_number");
+//                    int chapterCol= labelSpecificRows.getColumnIndex("chapter");
+//                    int verseFromCol= labelSpecificRows.getColumnIndex("verseFrom");
+//                    int verseToCol= labelSpecificRows.getColumnIndex("verseTo");
+//                    int noteCol= labelSpecificRows.getColumnIndex("note");
+//                    int _id = labelSpecificRows.getInt(_idCol);
+//                    String uuid = labelSpecificRows.getString(uuidCol);
+//                    String label_name = labelSpecificRows.getString(label_nameCol);
+//                    String label_color = labelSpecificRows.getString(label_colorCol);
+//                    int label_permanent = labelSpecificRows.getInt(label_perCol);
+//                    int book_number = labelSpecificRows.getInt(book_numberCol);
+//                    int chapter = labelSpecificRows.getInt(chapterCol);
+//                    int verseFrom = labelSpecificRows.getInt(verseFromCol);
+//                    int verseTo = labelSpecificRows.getInt(verseToCol);
+//                    String note = null;
+//                    if(!labelSpecificRows.isNull(noteCol)){
+//                        note = labelSpecificRows.getString(noteCol);
+//                    }
+//
+//                    Label specificLabel = new Label(label_id, label_name, label_color, label_permanent);
+//                    Book specificBook = BookHelper.getBook(book_number);
+//                    String innerQuery = "SELECT verse , text FROM verses WHERE " +
+//                            "book_number = "+book_number+" AND " +
+//                            "chapter = "+chapter+" AND " +
+//                            "verse BETWEEN "+verseFrom+" AND "+verseTo+" " +
+//                            "ORDER BY book_number, chapter, verse";
+////                    if(verseFrom == verseTo){
+////                        innerQuery = "SELECT verse , text FROM verses WHERE book_number = ? AND chapter = ? ORDER BY book_number, chapter, verse";
+////                    } else {
+////                        innerQuery = "SELECT verse , text FROM verses WHERE book_number = ? AND chapter = ? ORDER BY book_number, chapter, verse";
+////                    }
+////                    Cursor innerCursor = this.getReadableDatabase().rawQuery(innerQuery, null);
+//                    Integer  indexIfInHistory = history.get(uuid);
+//                    Cursor innerCursor = BibleDBHelper.getInstance(context).openDataBaseNoHelper(BibleDBHelper.DB_NAME_BIBLE_CONTENT).rawQuery(innerQuery, null);
+//                    if (innerCursor.moveToFirst()) {
+//                        do{
+//                            int verseCol = innerCursor.getColumnIndex(BibleContracts.VersesContract.COL_VERSE);
+//                            int textCol = innerCursor.getColumnIndex(BibleContracts.VersesContract.COL_TEXT);
+//                            int verse = innerCursor.getInt(verseCol);
+//                            String text = innerCursor.getString(textCol).trim();
+//                            //if one verse only OR this is the first verse from the query results.
+//                            //therefore, we'll only create one VersesMarked object and then add to it the rest of the verses.
+//                            if ((verseFrom == verseTo || verseFrom == verse) && (indexIfInHistory == null)) {
+//                                VersesMarked innerVerseMarked = new VersesMarked(_id, uuid, specificBook, specificLabel, chapter, verse, text, note);
+//                                list.add(innerVerseMarked);
+//                                history.put(uuid, (list.size() - 1));//put this as seen and the index where inserted in HISTORY
+//                            } else {//if more than one verse and this is not the first verse
+//                                ((VersesMarked) list.get(history.get(uuid))).addToVerseTextDict(verse, text);
+//                            }
+//                        } while(innerCursor.moveToNext());
+//                    }
+//
+//                    labelSpecificRows.moveToNext();
+//                }
+//            }
+//            labelSpecificRows.close();
+//        } catch (Exception e) {
+//        }
+//        return results;
+//    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE labels (_id INTEGER PRIMARY KEY, name VARCHAR NOT NULL, color VARCHAR NOT NULL, permanent INTEGER DEFAULT 0)");
@@ -335,9 +410,8 @@ public class ContentDBHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS [" + ContentDBContracts.LABELS.NAME + "];");
-        db.execSQL("DROP TABLE IF EXISTS [" + ContentDBContracts.VERSES_MARKED.NAME + "];");
-        db.execSQL("DROP TABLE IF EXISTS [" + ContentDBContracts.VERSES_LEARNED.NAME + "];");
+        db.execSQL("DROP TABLE IF EXISTS labels");
+        db.execSQL("DROP TABLE IF EXISTS verses_marked");
         onCreate(db);
     }
 
@@ -345,11 +419,5 @@ public class ContentDBHelper extends SQLiteOpenHelper {
     public void onConfigure(SQLiteDatabase db){
         db.setForeignKeyConstraintsEnabled(true);
     }
-
-    /**
-     * Provide access to our database.
-     */
-    public SQLiteDatabase getDb() {
-        return db;
-    }
 }
+
---
+++ app/src/main/java/com/zapatatech/santabiblia/DatabaseHelper/ContentDBContentProvider.java
@@ -0,0 +1,226 @@
+package com.zapatatech.santabiblia.DatabaseHelper;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class ContentDBContentProvider  extends ContentProvider {
+    // Use an int for each URI we will run, this represents the different queries
+    private static final int LABEL = 100;
+    private static final int LABEL_ID = 101;
+    private static final int VERSE_MARKED = 200;
+    private static final int VERSE_MARKED_ID = 201;
+    private static final int VERSE_LEARNED = 300;
+    private static final int VERSE_LEARNED_ID = 301;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private SQLiteDatabase db;
+
+    /**
+     * Builds a UriMatcher that is used to determine witch database request is being made.
+     */
+    public static UriMatcher buildUriMatcher(){
+        String content_authority = ContentDBContracts.CONTENT_AUTHORITY;
+        // All paths to the UriMatcher have a corresponding code to return, when a match is found (the ints above).
+        UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_LABELS, LABEL);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_LABELS + "/#", LABEL_ID);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_VERSES_MARKED, VERSE_MARKED);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_VERSES_MARKED + "/#", VERSE_MARKED_ID);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_VERSES_LEARNED, VERSE_LEARNED);
+        matcher.addURI(content_authority, ContentDBContracts.PATH_VERSES_LEARNED + "/#", VERSE_LEARNED_ID);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        db = ContentDBHelper.getInstance(getContext()).getDb();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Cursor c;
+        switch (sUriMatcher.match(uri)) {
+            //===============================================================================
+            // Query for multiple results
+            case LABEL:
+                c = db.query(ContentDBContracts.LABELS.NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            // Query for single result
+            case LABEL_ID:
+                long label_id = ContentUris.parseId(uri);
+                c = db.query(ContentDBContracts.LABELS.NAME,
+                        projection,
+                        ContentDBContracts.LABELS.COL_ID + "=?",
+                        new String[] { String.valueOf(label_id) },
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            //===============================================================================
+            // Query for multiple results
+            case VERSE_MARKED:
+                c = db.query(ContentDBContracts.VERSES_MARKED.NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            // Query for single result
+            case VERSE_MARKED_ID:
+                long verse_marked_id = ContentUris.parseId(uri);
+                c = db.query(ContentDBContracts.VERSES_MARKED.NAME,
+                        projection,
+                        ContentDBContracts.VERSES_MARKED.COL_ID + "=?",
+                        new String[] { String.valueOf(verse_marked_id) },
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            //===============================================================================
+            // Query for multiple results
+            case VERSE_LEARNED:
+                c = db.query(ContentDBContracts.VERSES_LEARNED.NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            // Query for single result
+            case VERSE_LEARNED_ID:
+                long verse_learned_id = ContentUris.parseId(uri);
+                c = db.query(ContentDBContracts.VERSES_LEARNED.NAME,
+                        projection,
+                        ContentDBContracts.VERSES_LEARNED.COL_ID + "=?",
+                        new String[] { String.valueOf(verse_learned_id) },
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            //===============================================================================
+            default: throw new IllegalArgumentException("Invalid URI!");
+        }
+
+        // Tell the cursor to register a content observer to observe changes to the URI or its descendants.
+        assert getContext() != null;
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    //The getType method is used to find the MIME type of the results, either a directory of multiple results, or an individual item
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch(sUriMatcher.match(uri)){
+            case LABEL:
+                return ContentDBContracts.LABELS.CONTENT_TYPE;
+            case LABEL_ID:
+                return ContentDBContracts.LABELS.CONTENT_ITEM_TYPE;
+            case VERSE_MARKED:
+                return ContentDBContracts.VERSES_MARKED.CONTENT_TYPE;
+            case VERSE_MARKED_ID:
+                return ContentDBContracts.VERSES_MARKED.CONTENT_ITEM_TYPE;
+            case VERSE_LEARNED:
+                return ContentDBContracts.VERSES_LEARNED.CONTENT_TYPE;
+            case VERSE_LEARNED_ID:
+                return ContentDBContracts.VERSES_LEARNED.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        Uri returnUri;
+        long _id;
+
+        switch (sUriMatcher.match(uri)) {
+            case LABEL:
+                _id = db.insert(ContentDBContracts.LABELS.NAME, null, values);
+                returnUri = ContentUris.withAppendedId(ContentDBContracts.LABELS.CONTENT_URI, _id);
+                break;
+            case VERSE_MARKED:
+                _id = db.insert(ContentDBContracts.VERSES_MARKED.NAME, null, values);
+                returnUri = ContentUris.withAppendedId(ContentDBContracts.VERSES_MARKED.CONTENT_URI, _id);
+                break;
+            case VERSE_LEARNED:
+                _id = db.insert(ContentDBContracts.VERSES_LEARNED.NAME, null, values);
+                returnUri = ContentUris.withAppendedId(ContentDBContracts.VERSES_LEARNED.CONTENT_URI, _id);
+                break;
+            default: throw new IllegalArgumentException("Invalid URI!");
+        }
+
+        // Notify any observers to update the UI
+        assert getContext() != null;
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int rows;
+        switch (sUriMatcher.match(uri)) {
+            case LABEL:
+                rows = db.delete(ContentDBContracts.LABELS.NAME, selection, selectionArgs);
+                break;
+            case VERSE_MARKED:
+                rows = db.delete(ContentDBContracts.VERSES_MARKED.NAME, selection, selectionArgs);
+                break;
+            case VERSE_LEARNED:
+                rows = db.delete(ContentDBContracts.VERSES_LEARNED.NAME, selection, selectionArgs);
+                break;
+            default: throw new IllegalArgumentException("Invalid URI!");
+        }
+
+        // Notify any observers to update the UI
+        if (rows != 0) {
+            assert getContext() != null;
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rows;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int rows;
+        switch (sUriMatcher.match(uri)) {
+            case LABEL:
+                rows = db.update(ContentDBContracts.LABELS.NAME, values, selection, selectionArgs);
+                break;
+            case VERSE_MARKED:
+                rows = db.update(ContentDBContracts.VERSES_MARKED.NAME, values, selection, selectionArgs);
+                break;
+            case VERSE_LEARNED:
+                rows = db.update(ContentDBContracts.VERSES_LEARNED.NAME, values, selection, selectionArgs);
+                break;
+            default: throw new IllegalArgumentException("Invalid URI!");
+        }
+        // Notify any observers to update the UI
+        if (rows != 0) {
+            assert getContext() != null;
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rows;
+    }
+}
